1b10058f046841c38bfde39e3615eaa0a61bb076
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2009-09-12 18:31:08 -0300

    Extract the test summaries to Report::Summaries
    This lets us reuse this handy methods in other reports by just including the mixin

--- lib/testicles/report.rb
+++ lib/testicles/report.rb
@@ -170,4 +170,116 @@ module Testicles
       alias_method :pending_message, :error_message
     end
   end
+
+  # Mixin that provides summaries for your text based test runs.
+  module Summaries
+    # Call on +:end+ to output the amount of tests (passed, pending, failed
+    # and errored), the amount of assertions, and the time elapsed.
+    #
+    # For example:
+    #
+    #     on :end do |report|
+    #       report.puts
+    #       report.summarize_test_totals
+    #     end
+    #
+    # This relies on the public Report API, and on the presence of a #puts
+    # method to write to whatever source you are writing your report.
+    def summarize_test_totals
+      puts test_totals
+      puts running_time
+    end
+
+    # Call on +:end+ to print a list of pending tests, including file and line
+    # number where the call to TestCase#pending+ was made.
+    #
+    # It will not output anything if there weren't any pending tests.
+    #
+    # For example:
+    #
+    #     on :end do |report|
+    #       report.puts
+    #       report.summarize_pending_tests
+    #     end
+    #
+    # This relies on the public Report API, and on the presence of a #puts
+    # method to write to whatever source you are writing your report.
+    def summarize_pending_tests
+      return if pendings.empty?
+
+      puts "Pending tests:"
+      puts
+
+      pad_indexes = pendings.size.to_s.size
+      pendings.each_with_index do |pending, index|
+        puts "  #{pad(index+1, pad_indexes)}) #{pending.test_name} (#{pending.pending_message})"
+        puts indent("On line #{pending.line} of `#{pending.file}'", 6 + pad_indexes)
+        puts
+      end
+    end
+
+    # Call on +:end+ to print a list of failures (failed assertions) and errors
+    # (unrescued exceptions), including file and line number where the test
+    # failed, and a short backtrace.
+    #
+    # It will not output anything if there weren't any pending tests.
+    #
+    # For example:
+    #
+    #     on :end do |report|
+    #       report.puts
+    #       report.summarize_pending_tests
+    #     end
+    #
+    # This relies on the public Report API, and on the presence of a #puts
+    # method to write to whatever source you are writing your report.
+    def summarize_errors
+      return if failures_and_errors.empty?
+
+      puts "Failures:"
+      puts
+
+      pad_indexes = failures_and_errors.size.to_s.size
+      failures_and_errors.each_with_index do |error, index|
+        puts "  #{pad(index+1, pad_indexes)}) #{test_type(error)}: `#{error.test_name}' (on line #{error.line} of `#{error.file}')"
+        puts indent("With `#{error.error_message}'", 6 + pad_indexes)
+        puts indent(error.backtrace[0..2].join("\n"), 6 + pad_indexes)
+        puts
+      end
+    end
+
+    private
+
+      def running_time
+        "Ran in #{time_elapsed} seconds"
+      end
+
+      def test_totals
+        "%d test%s, %d assertion%s (%d passed, %d pending, %d failed, %d errored)" % [total_tests,
+                                                                                      total_tests == 1 ? "" : "s",
+                                                                                      assertions,
+                                                                                      assertions == 1 ? "" : "s",
+                                                                                      passes.size,
+                                                                                      pendings.size,
+                                                                                      failures.size,
+                                                                                      errors.size]
+      end
+
+      def indent(strings, size=2, indent_with=" ")
+        Array(strings).map do |str|
+          str.to_s.split("\n").map {|s| indent_with * size + s }.join("\n")
+        end
+      end
+
+      def pad(str, amount)
+        " " * (amount - str.to_s.size) + str.to_s
+      end
+
+      def test_type(test)
+        case test # order is important since ErroredTest < FailedTest
+        when Report::ErroredTest; "Error"
+        when Report::FailedTest;  "Failure"
+        end
+      end
+  end
 end
--- lib/testicles/reports/progress.rb
+++ lib/testicles/reports/progress.rb
@@ -7,104 +7,44 @@ module Testicles
   # files and line numbers, and after that a list of all failures and errors,
   # which also contains the first 3 lines of the backtrace for each.
   class Reports::Progress < Report
+    include Summaries
+
     # Set the stream where the report will be written to. STDOUT by default.
     def initialize(stream=STDOUT)
       @stream = stream
     end
 
     on :end do |report|
-      report.instance_eval do
-        puts
-        puts
-        report_pending_tests unless pendings.empty?
-        report_errors unless errors.empty?
-        puts summary
-        puts running_time
-      end
+      report.puts
+      report.puts
+      report.summarize_pending_tests
+      report.summarize_errors
+      report.summarize_test_totals
     end
 
     on :pass do |report, pass|
-      report.send(:print, ".")
+      report.print "."
     end
 
     on :pending do |report, pending|
-      report.send(:print, "P")
+      report.print "P"
     end
 
     on :failure do |report, failure|
-      report.send(:print, "F")
+      report.print "F"
     end
 
     on :error do |report, error|
-      report.send(:print, "E")
+      report.print "E"
     end
 
-    private
+    def print(*args) #:nodoc:
+      @stream.print(*args)
+    end
 
-      def running_time
-        "Ran in #{time_elapsed} seconds"
-      end
-
-      def report_pending_tests
-        puts "Pending tests:"
-        puts
-
-        pad_indexes = pendings.size.to_s.size
-        pendings.each_with_index do |pending, index|
-          puts "  #{pad(index+1, pad_indexes)}) #{pending.test_name} (#{pending.pending_message})"
-          puts indent("On line #{pending.line} of `#{pending.file}'", 6 + pad_indexes)
-          puts
-        end
-      end
-
-      def report_errors
-        puts "Failures:"
-        puts
-
-        pad_indexes = failures_and_errors.size.to_s.size
-        failures_and_errors.each_with_index do |error, index|
-          puts "  #{pad(index+1, pad_indexes)}) #{test_type(error)}: `#{error.test_name}' (on line #{error.line} of `#{error.file}')"
-          puts indent("With `#{error.error_message}'", 6 + pad_indexes)
-          puts indent(error.backtrace[0..2].join("\n"), 6 + pad_indexes)
-          puts
-        end
-      end
-
-      def summary
-        "%d test%s, %d assertion%s (%d passed, %d pending, %d failed, %d errored)" % [total_tests,
-                                                                                      total_tests == 1 ? "" : "s",
-                                                                                      assertions,
-                                                                                      assertions == 1 ? "" : "s",
-                                                                                      passes.size,
-                                                                                      pendings.size,
-                                                                                      failures.size,
-                                                                                      errors.size]
-      end
-
-      def indent(strings, size=2, indent_with=" ")
-        Array(strings).map do |str|
-          str.to_s.split("\n").map {|s| indent_with * size + s }.join("\n")
-        end
-      end
-
-      def pad(str, amount)
-        " " * (amount - str.to_s.size) + str.to_s
-      end
-
-      def test_type(test)
-        case test # order is important since ErroredTest < FailedTest
-        when ErroredTest; "Error"
-        when FailedTest;  "Failure"
-        end
-      end
-
-      def print(*args)
-        @stream.print(*args)
-      end
-
-      def puts(*args)
-        @stream.puts(*args)
-      end
+    def puts(*args) #:nodoc:
+      @stream.puts(*args)
+    end
   end
 
   add_report :progress, Reports::Progress
