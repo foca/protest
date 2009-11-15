37e28e563a9224b124145cf3a5ead83130bfb16d
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2009-11-14 16:46:39 -0800

    BacktraceFilter's API matches that of Test::Unit
    This lets us take advantage of any extension to said backtrace
    filtering. For example letting us include rails built-in backtrace
    filter into our own.

--- lib/protest/utils/backtrace_filter.rb
+++ lib/protest/utils/backtrace_filter.rb
@@ -1,17 +1,25 @@
 module Protest
   module Utils
     # Small utility object to filter an error's backtrace and remove any mention
-    # of Testicle's own files.
-    module BacktraceFilter
-      # Path to the library's 'lib' dir.
-      BASE_PATH = /^#{Regexp.escape(File.dirname(File.dirname(File.dirname(File.expand_path(__FILE__)))))}/
+    # of Protest's own files.
+    class BacktraceFilter
+      ESCAPE_PATHS = [
+        # Path to the library's 'lib' dir.
+        /^#{Regexp.escape(File.dirname(File.dirname(File.dirname(File.expand_path(__FILE__)))))}/,
+      ]
+
+      # Convenience method to clean the API a bit
+      def self.filter(backtrace)
+        new.filter_backtrace(backtrace)
+      end
 
       # Filter the backtrace, removing any reference to files located in
       # BASE_PATH.
-      def self.filter(backtrace)
+      def filter_backtrace(backtrace, prefix=nil)
+        ESCAPE_PATHS << prefix unless prefix.nil?
         backtrace.reject do |line|
           file = line.split(":").first
-          File.expand_path(file) =~ BASE_PATH
+          ESCAPE_PATHS.any? {|path| File.expand_path(file) =~ path }
         end
       end
     end
